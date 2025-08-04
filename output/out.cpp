b51d2a42a750faa2ee7b456b9c3df0c480e4c758
Nico Q <73864341+FISHARMNIC@users.noreply.github.com>
2025-08-03 22:04:35 -0600

    added note for tomorrow

--- output/out.cpp
+++ output/out.cpp
@@ -2,20 +2,27 @@
 // Compiled with Construct 
 
 #include "include/js.hpp"
-
-js::dynamic a = static_cast<js::dynamic>(0);
-js::dynamic b = static_cast<js::dynamic>(0);
-js::dynamic c = static_cast<js::dynamic>(0);
+js::dynamic bob_version0__(js::dynamic a, js::number b);
+js::string bob_version1__(js::string a, js::string b);
+js::dynamic y = static_cast<js::dynamic>(0);
+js::string z ;
 
 int main() {
-  a = static_cast<js::dynamic>(static_cast<js::number>(2));
-  b = (((js::string("10") + static_cast<js::number>(10)) * a));
-  c = static_cast<js::dynamic>(static_cast<js::number>(100));
-  std::cout << c << std::endl;
-  a = static_cast<js::dynamic>(static_cast<js::number>(10));
-  c = (b);
-  std::cout << c << std::endl;
-  a = static_cast<js::dynamic>(js::string("Hello!"));
-  std::cout << a << std::endl;
+  y = (bob_version0__(static_cast<js::dynamic>(static_cast<js::number>(1)),
+                      (static_cast<js::number>(2))));
+  z = (bob_version1__((js::string("Hello ")), (js::string("World!"))));
+  std::cout << y << std::endl;
+  std::cout << z << std::endl;
   return 0;
+}
+
+js::dynamic bob_version0__(js::dynamic a, js::number b){
+  a = static_cast<js::dynamic>(js::string("aaaaa"));
+  js::dynamic c = ((a + b));
+  return ((c));
+}
+js::string bob_version1__(js::string a, js::string b){
+  a = (js::string("aaaaa"));
+  js::string c = ((a + b));
+  return ((c));
 }
--- tests/2.js
+++ tests/2.js
@@ -19,14 +19,14 @@ Similar to Test1, but "a" is also a "let" since it is retyped
 let a = 2;
 
 // Functions (no params no return yet)
-// function bob() {
-//     // Proper scope handling (this a != global a) 
-//     let a = 1.23;
-//     let b = "Hello";
+function bob() {
+    // Proper scope handling (this a != global a) 
+    let a = 1.23;
+    let b = "Hello";
 
-//     dbgprint(a);
-//     dbgprint(b);
-// }
+    dbgprint(a);
+    dbgprint(b);
+}
 
 // Proper JS type coercion (only for the 4 main math functions for now)
 // note that flipping these two decs causes lookahead issues since c is reassigned to b, which won't be declared
