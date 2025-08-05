11852039c494f2f5886b02749f9759eb867910a8
Nico Q <73864341+FISHARMNIC@users.noreply.github.com>
2025-08-04 23:10:19 -0600

    new test case

--- output/out.cpp
+++ output/out.cpp
@@ -2,23 +2,21 @@
 // Compiled with Construct 
 
 #include "include/js.hpp"
-void bob();
-js::number a ;
-js::number c ;
-js::number b ;
+void bob_version0__(js::array<js::number> a, js::number b);
+void bob_version1__(js::string a, js::number b);
 
 int main() {
-
-a = (static_cast<js::number>(2))
-c = (static_cast<js::number>(100))
-b = (((js::string("10")+static_cast<js::number>(10))*a))
-c = (b)
-std::cout << c << std::endl
-bob()
-return 0;
+  bob_version0__((js::array<js::number>({(static_cast<js::number>(2))})),
+                 (static_cast<js::number>(10)));
+  bob_version1__((js::string("5")), (static_cast<js::number>(2)));
+  return 0;
 }
-void bob(){js::number a = (static_cast<js::number>(1.23))
-js::string b = (js::string("Hello"))
-std::cout << a << std::endl
-std::cout << b << std::endl
+
+void bob_version0__(js::array<js::number> a, js::number b){
+  js::number c = ((a * b));
+  std::cout << c << std::endl;
+}
+void bob_version1__(js::string a, js::number b){
+  js::number c = ((a * b));
+  std::cout << c << std::endl;
 }
---
+++ tests/12.js
@@ -0,0 +1,8 @@
+function bob(a,b)
+{
+    let c = a * b;
+    dbgprint(c);
+}
+
+bob([2], 10);
+bob("5", 2);
