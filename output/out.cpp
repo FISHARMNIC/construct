9cf0bf4eb495c8a1f53f8c13cc5b7e0b019f1f86
Nico Q <73864341+FISHARMNIC@users.noreply.github.com>
2025-08-21 12:59:24 -0600

    added testcase for while loop

--- output/out.cpp
+++ output/out.cpp
@@ -2,14 +2,23 @@
 // Compiled with Construct 
 
 #include "include/js.hpp"
-
+void bob();
 js::number a ;
+js::number c ;
+js::number b ;
 
 int main() {
-  a = (static_cast<js::number>(1));
-  while ((a < static_cast<js::number>(10))) {
-    std::cout << a << std::endl;
-    a = ((a + static_cast<js::number>(1)));
-}
+
+a = (static_cast<js::number>(2))
+c = (static_cast<js::number>(100))
+b = (((js::string("10")+static_cast<js::number>(10))*a))
+c = (b)
+std::cout << c << std::endl
+bob()
 return 0;
+}
+void bob(){js::number a = (static_cast<js::number>(1.23))
+js::string b = (js::string("Hello"))
+std::cout << a << std::endl
+std::cout << b << std::endl
 }
