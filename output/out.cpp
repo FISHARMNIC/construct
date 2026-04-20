8b4198a1c1fa31a55b0cd28ea73d06a93c3ef789
FISHARMNIC <nicolas.quijano773@gmail.com>
2026-04-20 12:56:08 -0600

    passes all test cases

--- output/out.cpp
+++ output/out.cpp
@@ -2,19 +2,27 @@
 // Compiled with Construct 
 
 #include "../runtime/include/js.hpp"
-js::number bob();
+void bob();
 js::number a ;
-js::number q ;
+js::number b ;
+js::dynamic c = static_cast<js::dynamic>(0);
 
 int main() {
 _js_init_();
 
+a = (static_cast<js::number>(2));
+b = ((js::string("10") + static_cast<js::number>(10)) * a);
+c = static_cast<js::dynamic>(static_cast<js::number>(100));
+std::cout << c << std::endl;
 a = (static_cast<js::number>(10));
-q = (bob());
-std::cout << q << std::endl;
+c = static_cast<js::dynamic>(b);
+std::cout << c << std::endl;
+c = static_cast<js::dynamic>(js::string("Hello!"));
+std::cout << c << std::endl;
 return 0;
 }
-js::number bob(){
-  js::number c = a;
-  return c;
+void bob(){js::number a = (static_cast<js::number>(1.23));
+js::string b = (js::string("Hello"));
+std::cout << a << std::endl;
+std::cout << b << std::endl;
 }
